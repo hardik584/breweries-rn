3942eed8cba6fc768723d15937474951b0ef857d
Hardik Kumbhani <harkumbh1@WKMZTD12F7FF.global.publicisgroupe.net>
2022-09-20 13:46:44 +0530

    UI improvement

--- src/componants/BreweriesFlatList.js
+++ src/componants/BreweriesFlatList.js
@@ -1,46 +1,51 @@
-import React from "react";
-import {
-    FlatList,
-    Text,
-    View,
-    TouchableOpacity,
-  } from 'react-native';
+import React from 'react';
+import {FlatList, Text, View, TouchableOpacity, Image} from 'react-native';
 
 import CustomStyle from '../CustomStyle';
 
-const BreweriesFlatList = (props) => {
+const BreweriesFlatList = props => {
   return (
     <FlatList
-    data={props.data}
-    keyExtractor={({id}, index) => index}
-    renderItem={({item, index}) => (
-      <TouchableOpacity
-        onPress={() =>
+      data={props.data}
+      keyExtractor={({id}, index) => index}
+      renderItem={({item, index}) => (
+        <TouchableOpacity
+          onPress={() =>
             props.navigation.navigate('BreweriesDetails', {
-            breweriesItem: item,
-          })
-        }>
-        <View
-          style={[
-            CustomStyle.detailsBackgroundStyle,
-            {backgroundColor: index % 2 == 0 ? '#ddd' : 'white'},
-          ]}>
-          <View style={CustomStyle.viewRowStyle}>
-            <View style={CustomStyle.circleShape}>
-              <Text style={{align: 'center'}}> {index + 1} </Text>
+              breweriesItem: item,
+            })
+          }>
+          <View
+            style={[
+              CustomStyle.detailsBackgroundStyle,
+              {backgroundColor: index % 2 == 0 ? '#ddd' : 'white'},
+            ]}>
+            <View
+              style={[
+                CustomStyle.viewRowStyle,
+                {justifyContent: 'space-between', width: '100%'},
+              ]}>
+              <View style={{flexDirection: 'row',alignItems:'center'}}>
+                <View style={CustomStyle.circleShape}>
+                  <Text style={{align: 'center'}}> {index + 1} </Text>
+                </View>
+                <Text style={CustomStyle.baseText}>{item.name}</Text>
+              </View>
+              <Image
+                source={{
+                  uri: 'https://cdn-icons-png.flaticon.com/512/2990/2990159.png',
+                }}
+                style={[CustomStyle.imageStyle,{tintColor:"#38b6ba"}]}
+              />
             </View>
-            <Text style={CustomStyle.baseText}>{item.name} 👆</Text>
-
+            <Text>
+              {item.street}
+              {item.city}, {item.state}, {item.country}, {item.postal_code}
+            </Text>
           </View>
-          <Text>
-            {item.street}
-            {item.city}, {item.state}, {item.country},{' '}
-            {item.postal_code}
-          </Text>
-        </View>
-      </TouchableOpacity>
-    )}
-  />
+        </TouchableOpacity>
+      )}
+    />
   );
-}
+};
 export default BreweriesFlatList;
